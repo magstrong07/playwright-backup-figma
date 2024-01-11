ef9d8c625593e388d7a9d6472504fc3bd713f14d
Andrey Moskvin <moskowkach@gmail.com>
2024-01-12 01:28:31 +0300

    save fails in files

--- tests/download.spec.ts
+++ tests/download.spec.ts
@@ -4,31 +4,34 @@ import projectsMassive from "../data/fileUrl.json";
 
 test.describe("download", async () => {
   for (const chto of projectsMassive) {
-  let links = chto.files;
-  for (const index in links) {
-    const url = links[index].key;
-    test(links[index].key, async ({ page }) => {
-      test.setTimeout(1200000);
+    let links = chto.files;
+    for (const index in links) {
+      const url = links[index].key;
+      test(links[index].key, async ({ page }) => {
+        test.setTimeout(1200000);
 
-      await page.goto(`https://www.figma.com/file/${url}`);
+        await page.goto(`https://www.figma.com/file/${url}`);
 
-      await page.locator("id=toggle-menu-button").click();
-      await page.getByTestId("dropdown-option-File").click();
+        await page.locator("id=toggle-menu-button").click();
+        await page.getByTestId("dropdown-option-File").click();
 
-      try {
-        const downloadPromise = page.waitForEvent("download");
-        await page.getByTestId("dropdown-option-Save local copy…").click();
-        const download = await downloadPromise;
+        try {
+          const downloadPromise = page.waitForEvent("download");
+          await page.getByTestId("dropdown-option-Save local copy…").click();
+          const download = await downloadPromise;
 
-        await download.saveAs(
-          `./backups/${chto.name}/` + download.suggestedFilename()
-        );
-      } catch (e) {
-        const url = page.url();
-
-        await fs.appendFile("fail_download.txt", "\n" + url + "\n \n");
-      }
-    });
-  }
+          await download.saveAs(
+            `./backups/${chto.name}/` + download.suggestedFilename()
+          );
+        } catch (e) {
+          const url = page.url();
+          await fs.appendFile(
+            `./backups/${chto.name}/` + `${links[index].name}.txt`,
+            "файл нельзя скачать:" + "\n" + url + "\n \n"
+          );
+          await fs.appendFile("fail_download.txt", "\n" + url + "\n \n");
+        }
+      });
+    }
   }
 });
